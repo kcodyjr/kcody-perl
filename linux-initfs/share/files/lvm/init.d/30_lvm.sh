7273b0d68001c1e099f700460bcda0817dbd0b27
Kevin Cody-Little <kcodyjr@gmail.com>
2017-11-13 21:24:16 -0500

    Remove spaces from 30_lvm.sh

--- linux-initfs/share/files/lvm/init.d/30_lvm.sh
+++ linux-initfs/share/files/lvm/init.d/30_lvm.sh
@@ -1,17 +1,15 @@
 
-LVMALIAS='
-lvchange lvcreate lvextend lvmdiskscan lvmsar lvremove lvresize lvscan
-lvconvert lvdisplay lvmchange lvmconfig lvmsadc lvreduce lvrename lvs
+LVMALIAS='lvchange lvcreate lvextend lvmdiskscan lvmsar lvremove lvresize
+lvscan lvconvert lvdisplay lvmchange lvmconfig lvmsadc lvreduce lvrename lvs
 pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan
 vgcfgbackup vgchange vgconvert vgdisplay vgextend vgmknodes vgremove
 vgs vgsplit vgcfgrestore vgck vgcreate vgexport vgimport vgmerge
-vgreduce vgrename vgscan
-'
+vgreduce vgrename vgscan'
 
 for lvmalias in $LVMALIAS
 do
 	ln -s lvm /sbin/$lvmalias
 done
 
-unset lvmalias
+unset lvmalias LVMALIAS
 
