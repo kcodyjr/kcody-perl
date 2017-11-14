5dce33ada11b54cf43a9d0e647b27360aecefed9
Kevin Cody-Little <kcodyjr@gmail.com>
2017-11-13 21:10:16 -0500

    hush-friendly: remove array in 30_lvm.sh, change "source" to ".", and set a path early in /init

--- linux-initfs/share/files/lvm/init.d/30_lvm.sh
+++ linux-initfs/share/files/lvm/init.d/30_lvm.sh
@@ -1,14 +1,14 @@
 
-LVMALIAS=(
-	lvchange lvcreate lvextend lvmdiskscan lvmsar lvremove lvresize lvscan
-	lvconvert lvdisplay lvmchange lvmconfig lvmsadc lvreduce lvrename lvs
-	pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan
-	vgcfgbackup vgchange vgconvert vgdisplay vgextend vgmknodes vgremove
-	vgs vgsplit vgcfgrestore vgck vgcreate vgexport vgimport vgmerge
-	vgreduce vgrename vgscan
-)
+LVMALIAS='
+lvchange lvcreate lvextend lvmdiskscan lvmsar lvremove lvresize lvscan
+lvconvert lvdisplay lvmchange lvmconfig lvmsadc lvreduce lvrename lvs
+pvchange pvck pvcreate pvdisplay pvmove pvremove pvresize pvs pvscan
+vgcfgbackup vgchange vgconvert vgdisplay vgextend vgmknodes vgremove
+vgs vgsplit vgcfgrestore vgck vgcreate vgexport vgimport vgmerge
+vgreduce vgrename vgscan
+'
 
-for lvmalias in ${LVMALIAS[@]}
+for lvmalias in $LVMALIAS
 do
 	ln -s lvm /sbin/$lvmalias
 done
