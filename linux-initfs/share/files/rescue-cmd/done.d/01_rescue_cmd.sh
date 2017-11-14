1787dc1942bcfe5cc379fcf9b1ac1c2bbbf37af8
Kevin Cody-Little <kcodyjr@gmail.com>
2017-11-13 20:52:27 -0500

    Ignore parameter 'rescue' when the rootfs isn't mounted and the policy is to drop to a rescue shell anyway.

--- linux-initfs/share/files/rescue-cmd/done.d/01_rescue_cmd.sh
+++ linux-initfs/share/files/rescue-cmd/done.d/01_rescue_cmd.sh
@@ -1,6 +1,20 @@
 
-if [[ -n $RESCUE ]]
+unset rescue_doit
+
+if is_root_mounted
+then
+	rescue_doit=$RESCUE
+
+elif [[ -n $RESCUE &&
+        ( $ROOTFS_NOT_FOUND != rootfs_not_found_rescue ) ]]
+then
+	rescue_doit=nonempty
+fi
+
+if [[ -n $rescue_doit ]]
 then
 	rescue_shell
 fi
+
+unset rescue_doit
 
