4e33622fdcdfcc7d035a963a53e77aa51e5cd984
Kevin Cody-Little <kcodyjr@gmail.com>
2018-04-10 23:17:29 -0400

    rescue call fixes

--- linux-initfs/share/files/rescue/rescue_init.sh
+++ linux-initfs/share/files/rescue/rescue_init.sh
@@ -4,21 +4,20 @@
 
 rescue_shell() {
 	local rc
-	local run="setsid -c /bin/sh -l"
+	local run='setsid -c /bin/sh -l'
 	echo
-	echo "RESCUE SHELL: (exit or ^d to continue)"
+	echo 'RESCUE SHELL: (exit or ^d to continue)'
 	echo
 	(
 		export HOME=/root
 		cd $HOME
 		set +x
 		echo "+ $run" >&2 # force trace entry
-		$run </dev/console >/dev/console 2>/dev/console
-		set -x
+		$run </dev/console >/dev/console 2>&1
 	)
 	rc=$?
 	echo
-	echo "rescue shell returned $rc"
+	echo 'rescue shell returned' "$rc"
 	echo
 	return $rc
 }
