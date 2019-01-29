3ab20f1f768b9390a2af955a2b03b044e405f6fe
Kevin Cody-Little <kcodyjr@gmail.com>
2019-01-28 23:13:27 -0500

    derp, chmod before remount ro...

--- linux-initfs/share/files/core/done.d/preserve_initfs.sh
+++ linux-initfs/share/files/core/done.d/preserve_initfs.sh
@@ -4,7 +4,7 @@ INITFS_MNT="${FINDFS_MNT}/mnt/initfs"
 if [ -d "$INITFS_MNT" ]
 then
 	mount -o bind / "$INITFS_MNT"
-	mount -o ro,remount "$INITFS_MNT"
 	chmod 750 "$INITFS_MNT"
+	mount -o ro,remount "$INITFS_MNT"
 fi
 
